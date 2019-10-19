5f30f456ad8fe46b2c649451e84e47c3479748d7
Gina Leuty <ginaleuty15@gmail.com>
2019-10-18 21:20:27 -0500

    setup project

---
+++ src/main/java/org/launchcode/communitycookbook/CommunityCookbookApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.communitycookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityCookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityCookbookApplication.class, args);
+	}
+
+}
