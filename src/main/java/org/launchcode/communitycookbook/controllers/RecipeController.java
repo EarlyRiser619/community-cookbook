b41ab844341ecaeddbfb1d66a43cf2ff04fea8e5
Gina Leuty <ginaleuty15@gmail.com>
2019-10-19 12:23:28 -0500

    create addRecipe form and controllers to handle get and post requests to it

--- src/main/java/org/launchcode/communitycookbook/controllers/RecipeController.java
+++ src/main/java/org/launchcode/communitycookbook/controllers/RecipeController.java
@@ -4,16 +4,38 @@ package org.launchcode.communitycookbook.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("recipe")
 public class RecipeController {
 
+    static ArrayList<String> recipes = new ArrayList<>();
+
     @RequestMapping(value = "")
     public String index(Model model) {
+
+        model.addAttribute("recipes", recipes);
         model.addAttribute("title", "My Recipes");
 
-        return "recipes/index";
+        return "recipe/index";
+    }
+
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddRecipeForm(Model model) {
+        model.addAttribute("title", "Add Recipe");
+        return "recipe/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddRecipeForm(@RequestParam String recipeName){
+        recipes.add(recipeName);
+        return "redirect:";
     }
 
 }
