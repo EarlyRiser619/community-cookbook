f4d8fccf755f1c8ecd393904f621a439251daa2f
Gina Leuty <ginaleuty15@gmail.com>
2019-10-18 22:05:59 -0500

    add user controller, refactor controllers to link with index templates

--- src/main/java/org/launchcode/communitycookbook/controllers/RecipeController.java
+++ src/main/java/org/launchcode/communitycookbook/controllers/RecipeController.java
@@ -3,22 +3,14 @@ package org.launchcode.communitycookbook.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("recipe")
 public class RecipeController {
 
     @RequestMapping(value = "")
-    @ResponseBody
     public String index() {
-        return "My Recipes";
-    }
-
-    @RequestMapping(value = "goodbye")
-    @ResponseBody
-    public String goodbye() {
-        return "Goodbye";
+        return "recipes/index";
     }
 
 }
