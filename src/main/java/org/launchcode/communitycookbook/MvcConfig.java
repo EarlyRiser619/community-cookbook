32c4d8da2045d72889b9fc675595883f1b342be8
Gina Leuty <ginaleuty15@gmail.com>
2019-11-03 15:07:29 -0600

    start setup for spring security, config class

---
+++ src/main/java/org/launchcode/communitycookbook/MvcConfig.java
@@ -0,0 +1,17 @@
+package org.launchcode.communitycookbook;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/recipe/index.html").setViewName("home");
+        registry.addViewController("/recipe/add.html").setViewName("recipeAdd");
+        registry.addViewController("/user/index.html").setViewName("userHome");
+        registry.addViewController("/user/login.html").setViewName("login");
+        registry.addViewController("/user/add.html").setViewName("register");
+    }
+}
