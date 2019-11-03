4c9354d4bb0061ec395afd0a8ebd57a1534d45bd
Gina Leuty <ginaleuty15@gmail.com>
2019-11-03 17:39:05 -0600

    setup Spring Security dependencies and config

--- src/main/java/org/launchcode/communitycookbook/security/MvcConfig.java
+++ src/main/java/org/launchcode/communitycookbook/security/MvcConfig.java
@@ -1,4 +1,4 @@
-package org.launchcode.communitycookbook;
+package org.launchcode.communitycookbook.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
