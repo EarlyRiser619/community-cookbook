12962c7b0b89aa893f9c1da5a839935d997cb3a9
Gina Leuty <ginaleuty15@gmail.com>
2019-11-04 20:26:56 -0600

    add proper security html tag notations to all .html pages

--- src/main/java/org/launchcode/communitycookbook/security/WebSecurityConfig.java
+++ src/main/java/org/launchcode/communitycookbook/security/WebSecurityConfig.java
@@ -19,11 +19,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("home").permitAll()
+                    .antMatchers("/recipe/index", "/user/add").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
-                    .loginPage("/login")
+                    .loginPage("/user/login")
                     .permitAll()
                     .and()
                 .logout()
